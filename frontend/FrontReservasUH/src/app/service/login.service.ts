164b0e4af5e0c3bf4040c9be93c088a1c46a602e
Molina211 <150742035+Molina211@users.noreply.github.com>
2025-05-17 04:01:48 -0500

    feature[HU-04] 🔄 Verification code to the email

--- frontend/FrontReservasUH/src/app/service/login.service.ts
+++ frontend/FrontReservasUH/src/app/service/login.service.ts
@@ -1,10 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
-import { Observable, Subject } from 'rxjs';
-=======
 import { Observable } from 'rxjs';
->>>>>>> feature/HU-04
 import { API_BASE_URL } from './api-url';
 
 @Injectable({
@@ -17,8 +13,6 @@ export class LoginService {
   login(endpoint: string, credentials: { correo: string; contrasena: string }): Observable<any> {
     return this.http.post<any>(`${API_BASE_URL}/${endpoint}`, credentials);
   }
-<<<<<<< HEAD
-=======
 
   sendEmail(endpoint: string, correo: string): Observable<any> {
   return this.http.post(`${API_BASE_URL}/${endpoint}`, { correo }, { responseType: 'text' });
@@ -30,5 +24,4 @@ export class LoginService {
     responseType: 'text'
   }) as Observable<string>;
 }
->>>>>>> feature/HU-04
 }
